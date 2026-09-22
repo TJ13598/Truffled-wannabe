6bc1152ee5469474b06dcd7da6cd19f9192b436a
TrevorJ375 <trevor1015339@students.sbcsc.k12.in.us>
2026-09-22 10:34:16 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,93 @@
+/*
+=========================================================
+ADD YOUR GAMES HERE
+=========================================================
+
+Each game has:
+
+name     = Name shown underneath the picture
+image    = Your thumbnail/photo
+iframe   = URL of the game
+category = Used by the category filter
+
+Example:
+
+{
+    name: "My Game",
+    image: "images/my-game.jpg",
+    iframe: "https://example.com/game",
+    category: "arcade"
+}
+
+=========================================================
+*/
+
+
+const games = [
+
+    {
+        name: "My First Game",
+        image: "images/game1.jpg",
+        iframe: "https://example.com/game1",
+        category: "arcade"
+    },
+
+    {
+        name: "My Second Game",
+        image: "images/game2.jpg",
+        iframe: "https://example.com/game2",
+        category: "action"
+    },
+
+    {
+        name: "My Third Game",
+        image: "images/game3.jpg",
+        iframe: "https://example.com/game3",
+        category: "puzzle"
+    },
+
+    {
+        name: "My Fourth Game",
+        image: "images/game4.jpg",
+        iframe: "https://example.com/game4",
+        category: "sports"
+    },
+
+    {
+        name: "My Fifth Game",
+        image: "images/game5.jpg",
+        iframe: "https://example.com/game5",
+        category: "action"
+    },
+
+    {
+        name: "My Sixth Game",
+        image: "images/game6.jpg",
+        iframe: "https://example.com/game6",
+        category: "arcade"
+    },
+
+    {
+        name: "My Seventh Game",
+        image: "images/game7.jpg",
+        iframe: "https://example.com/game7",
+        category: "puzzle"
+    },
+
+    {
+        name: "My Eighth Game",
+        image: "images/game8.jpg",
+        iframe: "https://example.com/game8",
+        category: "sports"
+    },
+
+    {
+        name: "My Ninth Game",
+        image: "images/game9.jpg",
+        iframe: "https://example.com/game9",
+        category: "arcade"
+    },
+
+    {
+        name: "My Tenth Game",
+        image: "images
